74ffef49c221bd215940f971185c39c4acf29295
erixweb <erikmorenodev@gmail.com>
2023-04-07 12:07:32 +0200

    Add like button

--- src/Components/FurnitureList.jsx
+++ src/Components/FurnitureList.jsx
@@ -1,41 +1,14 @@
-export default function FurnitureList (props) {
-    const { filter } = props 
+import Heart from "../assets/Heart"
+import Redirect from "../assets/Redirect"
+import furniture from "../store"
+
+
+export default function FurnitureList(props) {
+
+    const { filter } = props
+
 
     let filterFurniture = ""
-    const furniture = [
-        {
-            id: 1,
-            name: "Sofa 200",
-            image: "https://cdn.sklum.com/es/1274432/sofa-cama-de-3-plazas-en-lino-brion.jpg",
-            price: "€359",
-            desc: "Sofa grande de 400cm de longitud y 60cm de ancho",
-            medidas: "400cm * 60cm",
-        },
-        {
-            id: 2,
-            name: "Mesa grande",
-            image: "https://cdn2.jysk.com/getimage/wd2.medium/159399",
-            price: "€140",
-            desc: "Mesa de 10 metros de longitud y 2m de ancho",
-            medidas: "1000cm * 200cm",
-        },
-        {
-            id: 3,
-            name: "Lampara",
-            image: "https://www.gurilibis.com/wp-content/uploads/2020/03/LA%CC%81MPARA_DE_SOBREMESA_SPIELBERG_CAFE_NEGRO_02.jpg",
-            price: "€40",
-            desc: "Lampara eléctrica con diferentes tipos de luz",
-            medidas: "20cm * 20cm",
-        },
-        {
-            id: 4,
-            price: "€40",
-            name: "Mesa pequeña para comedor",
-            image: "https://hipopotamo.com/wp-content/uploads/2019/05/mesa-comedor-campestre-recta-madera-maciza-rustico-24312111.jpg",
-            desc: "Mesa pequeña para comedor muy barata",
-            medidas: "60cm * 60cm",
-        },
-    ]
     if (filter !== "") {
         filterFurniture = furniture.filter(r => r.desc.toLowerCase().includes(filter.toLowerCase()) || r.name.toLowerCase().includes(filter.toLowerCase()))
     }
@@ -44,7 +17,7 @@ export default function FurnitureList (props) {
         <section className="furniture-wrap">
             {filterFurniture == "" ?
                 furniture.map(item => (
-                    <a id={item.id}>
+                    <article id={item.id}>
                         <img src={item.image} alt={`Imagen del mueble ${item.name}`} />
                         <div className="content">
                             <h2>{item.name}</h2>
@@ -54,24 +27,32 @@ export default function FurnitureList (props) {
                             <p>
                                 {item.desc}
                             </p>
+                            <Heart />
+                            <button type="button">
+                                <Redirect />
+                            </button>   
                         </div>
-                    </a>
+                    </article>
                 ))
-            :
-            furniture.filter(r => r.desc.toLowerCase().includes(filter.toLowerCase()) || r.name.toLowerCase().includes(filter.toLowerCase())).map(item => (
-                <a id={item.id}>
-                    <img src={item.image} alt={`Imagen del mueble ${item.name}`} />
-                    <div className="content">
-                        <h2>{item.name}</h2>
-                        <small>
-                            {item.price}
-                        </small>
-                        <p>
-                            {item.desc}
-                        </p>
-                    </div>
-                </a>
-            ))
+                :
+                furniture.filter(r => r.desc.toLowerCase().includes(filter.toLowerCase()) || r.name.toLowerCase().includes(filter.toLowerCase())).map(item => (
+                    <article id={item.id}>
+                        <img src={item.image} alt={`Imagen del mueble ${item.name}`} />
+                        <div className="content">
+                            <h2>{item.name}</h2>
+                            <small>
+                                {item.price}
+                            </small>
+                            <p>
+                                {item.desc}
+                            </p>
+                            <Heart /> 
+                            <button type="button">
+                                <Redirect />
+                            </button>
+                        </div>
+                    </article>
+                ))
             }
         </section>
     )
